b03e56d019d1d73f7e038515c953891dbcf2c406
vijayaram bethina <vijayarambethina@vijayarams-Mac-mini.local>
2018-12-14 13:12:16 -0600

    Wireup alexa stuff

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+var Alexa = require('alexa-sdk');
+
 exports.handler = async (event) => {
-    // TODO implement
-    console.log('Version 1.0 of testing code build');
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit(':ask', 'Welcome to AWS Developer Certification!', 'Try saying hello!');
+    },
+    'NewSession': function () {
+        this.emit(':ask', 'Welcome to AWS Developer Certification Skill. Are you ready');
+    },   
+}
