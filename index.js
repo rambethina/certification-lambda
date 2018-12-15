d473aa033bd263b049bc8e3e67544e773185401d
vijayaram bethina <vijayarambethina@vijayarams-Mac-mini.local>
2018-12-14 18:16:05 -0600

    back to basics

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var Alexa = require('alexa-sdk');
+// var Alexa = require('alexa-sdk');
 
 exports.handler = async (event, context, callback) => {
-    console.log('Executing Lambda ....')
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+    console.log('Executing Lambda ....');
+    // var alexa = Alexa.handler(event, context);
+    // alexa.registerHandlers(handlers);
+    // alexa.execute();
 };
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit(':ask', 'Welcome to AWS Developer Certification!', 'Try saying hello!');
-    },
-    'NewSession': function () {
-        this.emit(':ask', 'Welcome to AWS Developer Certification Skill. Are you ready');
-    },   
-}
+// var handlers = {
+//     'LaunchRequest': function () {
+//         this.emit(':ask', 'Welcome to AWS Developer Certification!', 'Try saying hello!');
+//     },
+//     'NewSession': function () {
+//         this.emit(':ask', 'Welcome to AWS Developer Certification Skill. Are you ready');
+//     },   
+// }
